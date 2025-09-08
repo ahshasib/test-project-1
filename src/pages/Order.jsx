b30b0a9aff1bee130200a5ee07b58abec651853d
ahshasib <154332574+ahshasib@users.noreply.github.com>
2025-09-08 23:51:35 +0600

    update order page

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Order = () => {
   return (
-    <div className='bg-gray-800 h-screen text-white'>Order</div>
+    <div className='bg-gray-800 h-screen '>
+      <h1 className='text-white text-5xl'>this my order page</h1>
+    </div>
   )
 }
 
