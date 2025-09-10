cc4556ebc18a9b5523359fe89c2a190ee4c49800
ahshasib <154332574+ahshasib@users.noreply.github.com>
2025-09-10 23:14:54 +0600

    add a title

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -4,6 +4,7 @@ const Order = () => {
   return (
     <div className='bg-gray-800 h-screen '>
       <h1 className='text-white text-5xl'>this my order page</h1>
+      <h1 className='text-white text-5xl'>this my order page</h1>
     </div>
   )
 }
